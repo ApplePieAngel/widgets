c0a8d63b055e2f1bb700bbba781799344f44449d
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2022-05-18 13:30:42 -0400

    Added active functionality to Accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
+    const [activeIndex, setActiveIndex] = useState(null);
     const onTitleClick = (index) => {
-        console.log('Title clicked', index);
+        setActiveIndex(index);
     };
 
 
     const renderedItems = items.map((item, index) => {
-        return <React.Fragment key={item.title}>
-            <div className="title active" onClick={() => onTitleClick(index)}>
+        const active = index === activeIndex ? 'active' : '';
+
+        return (<React.Fragment key={item.title}>
+            <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
                 <i className="dropdown icon"></i>
                 {item.title}
             </div>
-            <div>
-                <div className="content active">
-                    <p>{item.content}</p>
-                </div>
+            <div className={`content ${active}`}>
+                <p>{item.content}</p>
             </div>
         </React.Fragment>
+        );
     });
     return <div className="ui styled accordion">
         {renderedItems}
