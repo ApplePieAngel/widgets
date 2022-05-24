7acb2975eeb99185d6353d7cbece2669e8291e62
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2022-05-24 16:33:31 -0400

    Added a timeout feature to the wikipedia search API function

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,7 +6,6 @@ const Search = () => {
     const [results, setResults] = useState([]);
 
 
-
     useEffect(() => {
         const search = async () => {
             const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
@@ -21,9 +20,39 @@ const Search = () => {
             setResults(data.query.search);
         };
 
-        search();
+        if (term && !results.length) {
+            search();
+        }
+        else {
+            const timeOutId = setTimeout(() => {
+                if (term) {
+                    search();
+                }
+            }, 500);
+
+            return () => {
+                clearTimeout(timeOutId);
+            };
+        }
+
 
     }, [term]);
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+                </div>
+            </div>
+        );
+    })
 
     return (
         <div>
@@ -35,6 +64,9 @@ const Search = () => {
                         onChange={e => setTerm(e.target.value)}
                         className="input" />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
             </div>
         </div>
     );
