159c013d9dbf11cb05b38edffb9086eaddb5ba90
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2022-06-08 14:27:18 -0400

    Added Dropdown component intial code

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,8 +3,18 @@ import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('programming');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
 
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [term]);
 
     useEffect(() => {
         const search = async () => {
@@ -14,29 +24,13 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term,
+                    srsearch: debouncedTerm,
                 },
             });
             setResults(data.query.search);
         };
-
-        if (term && !results.length) {
-            search();
-        }
-        else {
-            const timeOutId = setTimeout(() => {
-                if (term) {
-                    search();
-                }
-            }, 500);
-
-            return () => {
-                clearTimeout(timeOutId);
-            };
-        }
-
-
-    }, [term]);
+        search();
+    }, [debouncedTerm]);
 
     const renderedResults = results.map((result) => {
         return (
